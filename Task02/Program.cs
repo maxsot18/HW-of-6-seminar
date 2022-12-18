0fd23bac701231bc6418d954c6597c3dd6b4ea74
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-18 06:08:56 -0800

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, 
+// заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Prompt(string message)
+{
+    Console.WriteLine($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+double FindDot(double b1, double k1)
+{
+    double x = -0.5;
+    double y = k1 * x + b1;
+    
+    return y;
+    
+}
+double b1 = Prompt("Enter a value of B1");
+double k1 = Prompt("Enter a value of K1");
+double b2 = Prompt("Enter a value of B2 ");
+double k2 = Prompt("Enter a value of K2");
+double y = FindDot(b1, k1);
+double y1 = FindDot(b2, k2);
+Console.Write($"{y} {y1}");
